438e9e0dc93febacef14a9575985a6f5e4453547
Olive Zhang <xingwang.zhang@everbridge.com>
2020-12-20 10:29:49 +0800

    first workflow

---
+++ 101xin/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        contentPath: 'content/posts'
+      },
+    },
+  ],
+  // ➜  posts git:(master) ✗ ln -snf /home/zhangolve/github/my-blog/101xin ./
+  // Customize your site metadata:
+
+  // cp -r dir1/* dir2
+
+  siteMetadata: {
+    title: `101封信`,
+    author: `爱你的zxw`,
+    description: `写给宝贝的101封信`,
+    social: [
+     
+    ],
+  },
+}
