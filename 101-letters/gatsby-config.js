f9d47e86456b0271cd751e781ea9f8f1c166b6cd
zhangolve <zhangolve@gmail.com>
2021-11-23 17:34:56 +0800

    test

--- 101-letters/gatsby-config.js
+++ 101-letters/gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       resolve: `gatsby-theme-blog`,
       options: {
-        contentPath: 'content/posts/101xin'
+        contentPath: 'content/posts',
       },
-    },
+    }
   ],
   // ➜  posts git:(master) ✗ ln -snf /home/zhangolve/github/my-blog/101xin ./
   // Customize your site metadata:
@@ -15,9 +15,6 @@ module.exports = {
   siteMetadata: {
     title: `101封信`,
     author: `爱你的zxw`,
-    description: `写给宝贝的101封信`,
-    social: [
-     
-    ],
+    description: `写给宝贝的101封信`
   },
 }
---
+++ 101-letters/src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,14 @@
+const darkBlue = `#007acc`
+// const lightBlue = `#66E0FF`
+const blueGray = `#282c35`
+ const darkGray = `#f7f7f7`
+ const theme = {
+  colors: {
+    text: blueGray,
+    primary: darkBlue,
+    heading: blueGray,
+    background: darkGray
+  }
+}
+
+export default theme;
