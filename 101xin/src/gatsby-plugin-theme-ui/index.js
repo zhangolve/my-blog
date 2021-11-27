e3f8f5da53c5f90512b8987b2e682e799610ca41
Olive Zhang <xingwang.zhang@everbridge.com>
2021-11-27 19:55:01 +0800

    update workfow

--- 101xin/src/gatsby-plugin-theme-ui/index.js
+++ 101xin/src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,8 @@
 const darkBlue = `#007acc`
 // const lightBlue = `#66E0FF`
 const blueGray = `#282c35`
- const darkGray = `#f7f7f7`
- const theme = {
+const darkGray = `#f7f7f7`
+const theme = {
   colors: {
     text: blueGray,
     primary: darkBlue,
