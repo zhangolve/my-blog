4ab0e29321a5f012e5f8479523c85ae373fa6d42
Olive Zhang <xingwang.zhang@everbridge.com>
2021-12-04 20:44:04 +0800

    today blog

---
+++ today/mail_sender.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+import smtplib
+import time
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+
+
+class MailSender(object):
+
+	def __init__(self,my_sender,my_pass,sender_name,receiver_addr,subject,content, attachment):
+		self.my_sender=my_sender
+		self.my_pass=my_pass#口令，不是密码，通常为16位字符串
+		self.sender_name=sender_name
+		self.receiver_addr=receiver_addr
+		self.subject=subject
+		self.content=content
+		self.attachment=attachment
+		
+	def send_it(self):
+		msg = MIMEMultipart()
+		msg.attach(MIMEText(self.content,'plain','utf-8',))
+		msg['From']=formataddr([self.sender_name,self.my_sender])
+		msg['to']='管理员'  
+		msg['Subject']=self.subject
+		if self.attachment: 
+			with open(self.attachment, 'rb') as f:
+				# set attachment mime and file name, the image type is png
+				mime = MIMEBase('image', 'png', filename='img1.png')
+				# add required header data:
+				mime.add_header('Content-Disposition', 'attachment', filename='img1.png')
+				mime.add_header('X-Attachment-Id', '0')
+				mime.add_header('Content-ID', '<0>')
+				# read attachment file content into the MIMEBase object
+				mime.set_payload(f.read())
+				# encode with base64
+				encoders.encode_base64(mime)
+				# add MIMEBase object to MIMEMultipart object
+				msg.attach(mime)
+		server=smtplib.SMTP_SSL("smtp.qq.com", 465)
+		server.login(self.my_sender, self.my_pass)
+		server.sendmail(self.my_sender,self.receiver_addr,msg.as_string())
+		server.quit()
+		print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'邮件发送成功')
+
+
+		
---
+++ today/today.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from datetime import date
+from mail_sender import MailSender
+
+today = date.today()
+d1 = today.strftime("-%m-%d")
+
+my_sender = '1262010981@qq.com'
+my_pass = 'nrrejsviolzpjchd'
+receiver_addr = ['zhangolve@gmail.com']
+sender_name = 'History blog'
+subject = 'history blog ' + today.strftime("%Y-%m-%d %H:%M:%S")
+
+
+
+working_dir = Path()
+contents = []
+for path in working_dir.glob("../**/*.md"):
+    # include md file / weibo / txt csv ... files 
+    # sort by date
+    # html to markdown
+    md_blogs = []
+    txt_blogs = []
+    weibo = []
+    with open(path.absolute()) as f:
+        content = f.read()
+        if d1 in content:            
+            contents.append(content)
+contents = '\n\n\n'.join(contents)
+if not contents:
+    contents = 'there is no history blog'
+
+
+mailsender=MailSender(my_sender, my_pass, sender_name, receiver_addr, subject, contents, None)
+mailsender.send_it()
