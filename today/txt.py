9ea69ec8c07c8cf3b2337833bb797b2d74057038
Olive Zhang <xingwang.zhang@everbridge.com>
2021-12-10 13:51:01 +0800

    update 2021

--- today/txt.py
+++ today/txt.py
@@ -3,24 +3,22 @@ import re
 from datetime import date
 
 today = date.today()
-d1 = today.strftime("-%m-%d")
-
+# d1 = today.strftime("-%m-%d")
+d1 = '-05-18'
 
 working_dir = Path()
 contents = []
 for path in working_dir.glob("../*.txt"):
     with open(path.absolute(), encoding="utf-8") as f:
         content = f.read()
-        my_regex = r"\d{4}-05-18"  + r"(.+)((?:\n.+)+)(\d{4}-\d{2}-\d{2})"
-        m= re.findall(my_regex, content)
-        print(m)
+        rex = r"(.*?)\n.*?\d{4}" + r'{}'.format(d1)+ r"(.+)((?:\n.+)+)\d{4}-\d{2}-\d{2}"
+        print(rex)
+        all= re.findall(rex, content)
+        for i in all:
+            print(i)
     print(path.absolute())
 
 
-TEXTO = "Vaeeer"
-subject = r"Var\boundary"
+# str="Test String which should \n end here \n Test  here String which should end here"
 
-if re.search(rf"\b(?=\w){TEXTO}\\boundary(?!\w)", subject, re.IGNORECASE):
-    print("match")
-
-# print(re.search(rf"\b(?=\w){TEXTO}\\boundary(?!\w)", subject), re.IGNORECASE)
+# print(re.findall(r'String which\b\n{0,}\b.*?here?', str))
