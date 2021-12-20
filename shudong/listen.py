52393cc5966c174534d106aaa23a15644e87c60e
Olive Zhang <xingwang.zhang@everbridge.com>
2021-12-20 17:09:19 +0800

    update search

--- shudong/listen.py
+++ shudong/listen.py
@@ -23,6 +23,7 @@ from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import leancloud
 from fire import upload_blob
+from search import search
 leancloud.init("rkX7RdhzjQ0DdnpkcffRn4TD-gzGzoHsz", "pbWq8UDhvWbRpjebqfhqj4pG")
 
 Shudong = leancloud.Object.extend('shudong')
@@ -74,6 +75,32 @@ def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
 
+def could_search(update: Update, context: CallbackContext):
+    update.message.reply_text('清输入搜索内容')
+    return 0
+
+def search_text(update: Update, context: CallbackContext):
+    tweet_list = search(update.message.text)
+    reply_content = ''
+    if tweet_list:
+        for tweet in tweet_list:
+            reply_content = reply_content + f'{tweet["tweet"]["full_text"]}\n' + f'{tweet["tweet"]["created_at"]}\n'
+    else:
+        reply_content = '没有找到相关内容'
+    update.message.reply_text(reply_content)
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
@@ -85,10 +112,21 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("search", help_command))
 
     # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
     dispatcher.add_handler(MessageHandler(Filters.photo & ~Filters.command, upload_photo))
+        
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('search', could_search)],
+        states={
+            0: [MessageHandler(Filters.text, search_text)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+    dispatcher.add_handler(conv_handler)    
+
     # Start the Bot
     updater.start_polling()
 
---
+++ shudong/search.py
@@ -0,0 +1,35 @@
+import json
+
+
+def search_in_shudong(text):
+    result = []
+    with open('../shudong_data/leancloud/data.json') as fp:
+        data = json.load(fp)
+        if data:
+            for shudong in data:
+                full_text = shudong['tweet']['full_text']
+                if text in full_text:
+                    result.append(shudong)
+    return result
+
+
+def search_in_tweet(text):
+    result = []
+    with open('../shudong_data/leancloud/tweet.js') as fp:
+        # fp as a string
+        data = fp.read()
+        tweets = json.loads(data[25:])
+        if tweets:
+            for tweet in tweets:
+                full_text = tweet['tweet']['full_text']
+                if text in full_text:
+                    result.append(tweet)
+    return result
+
+
+def search(text):
+    result = []
+    result.extend(search_in_shudong(text))
+    result.extend(search_in_tweet(text))
+    return result
+
