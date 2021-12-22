a96fefe12b536afe8d2fd880c500aa501262bf2f
Olive Zhang <xingwang.zhang@everbridge.com>
2021-12-22 16:20:43 +0800

    update listener

--- shudong/fire.py
+++ shudong/fire.py
@@ -15,6 +15,8 @@ def upload_blob(source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name, content_type='image/jpeg')
+    blob.make_public()
+    return blob.public_url
     # blob.upload_from_file(file)
 
 
--- shudong/listen.py
+++ shudong/listen.py
@@ -36,6 +36,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+THREAD_ARR = []
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -48,17 +49,33 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def upload_photo(update: Update, context: CallbackContext) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
+
+def write_single_shudong(text):
+    shudong = Shudong()
+    shudong.set('content', text)
+    shudong.save()
+
+
+def upload_single_photo(photo_file):
     file_unique_id = photo_file['file_unique_id']
     file_name = '{}.jpg'.format(file_unique_id)
     photo_file.download(file_name)
-    upload_blob('./{}'.format(file_name), file_name)
+    url = upload_blob('./{}'.format(file_name), file_name)
+    return url
+
+
+def upload_photo(update: Update, context: CallbackContext) -> int:
+    """Stores the photo and asks for a location."""
+    caption = update.message.caption or ''
+    photo_file = update.message.photo[-1].get_file()
+    url = upload_single_photo(photo_file)
+    shudong = caption + '\n' + url
+    write_single_shudong(shudong)
+    update.message.reply_text(shudong)
     update.message.reply_text(
         'photo uploaded'
     )
+    return shudong
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -66,18 +83,17 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+
+def write_shudong(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    print(update.message)
-    shudong = Shudong()
-    shudong.set('content', update.message.text)
-    shudong.save()
-    update.message.reply_text(update.message.text)
+    write_single_shudong(update.message.text)
+    update.message.reply_text('content saved')
 
 
 def could_search(update: Update, context: CallbackContext):
     update.message.reply_text('清输入搜索内容')
     return 0
+
 
 def search_text(update: Update, context: CallbackContext):
     tweet_list = search(update.message.text)
@@ -88,6 +104,36 @@ def search_text(update: Update, context: CallbackContext):
     else:
         reply_content = '没有找到相关内容'
     update.message.reply_text(reply_content)
+    return ConversationHandler.END
+
+
+# thread logic
+
+def could_thread(update: Update, context: CallbackContext):
+    global THREAD_ARR 
+    THREAD_ARR = []
+    update.message.reply_text('请开始你的表演')
+    return 0
+
+
+def tell_thread(update: Update, context: CallbackContext):
+    if update.message.text:
+        THREAD_ARR.append(update.message.text)
+    if update.message.photo:
+        photo_file = update.message.photo[-1].get_file()
+        url = upload_single_photo(photo_file)
+        THREAD_ARR.append(url)
+    update.message.reply_text('请继续你的表演')
+    return 0
+
+
+def thread_create(update: Update, context: CallbackContext):
+    print('create')
+    global THREAD_ARR 
+    shudong = '\n'.join(THREAD_ARR)
+    write_single_shudong(shudong)
+    update.message.reply_text('preview shudong:'+ shudong)
+    THREAD_ARR= []
     return ConversationHandler.END
 
 
@@ -106,6 +152,8 @@ def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater("5065608320:AAHV4EkYAW9mFRK6QwWtAonI8nZ83LVeQSI")
+    # dev bot
+    # updater = Updater("5064508364:AAEfTF9IlNUwIrEaL3VPWn0WsQsgb4bhzNA")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -124,8 +172,20 @@ def main() -> None:
     )
     dispatcher.add_handler(conv_handler)    
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    thread_handler = ConversationHandler(
+        entry_points=[CommandHandler('thread', could_thread)],
+        states={
+            0: [
+                MessageHandler((Filters.text | Filters.photo) & ~Filters.command, tell_thread), 
+                CommandHandler('enter', thread_create)
+            ],
+        },
+        fallbacks=[CommandHandler('enter', thread_create)],
+    )
+    dispatcher.add_handler(thread_handler)    
+
+    # on non command i.e message - write_shudong the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, write_shudong))
     dispatcher.add_handler(MessageHandler(Filters.photo & ~Filters.command, upload_photo))
     
     # Start the Bot
