e63f4ad495cd12b0ebcbbf6702bda4f77327deae
zhangolve <zhangolve@users.noreply.github.com>
2021-12-31 16:36:49 +0800

    Update fire.py

--- shudong/fire.py
+++ shudong/fire.py
@@ -13,7 +13,7 @@ def upload_blob(source_file_name, destination_blob_name):
     bucket = storage.bucket()
 
     blob = bucket.blob(destination_blob_name)
-
+    blob.chunk_size = 5 * 1024 * 1024 # Set 5 MB blob size
     blob.upload_from_filename(source_file_name, content_type='image/jpeg', timeout=(6.05, 60), num_retries=5)
     blob.make_public()
     return blob.public_url
