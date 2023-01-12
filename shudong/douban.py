f7a2771ac096e9278e73c68fd859b335655b7bff
zhangolve <zhangolve@users.noreply.github.com>
2023-01-12 06:27:02 +0000

    update rss feed write

--- shudong/douban.py
+++ shudong/douban.py
@@ -1,23 +1,59 @@
-# https://rsshub.app/douban/people/62759792/status
-
+import leancloud
 import feedparser
+import os
+from bs4 import BeautifulSoup
+import datetime
 
 # 解析豆瓣电影的rss feed文本
 douban_rss = feedparser.parse('https://rsshub.app/douban/people/71828508/status')
 
+leancloud.init("rkX7RdhzjQ0DdnpkcffRn4TD-gzGzoHsz", "pbWq8UDhvWbRpjebqfhqj4pG")
+
+
+
+if os.environ.get('USER') == 'zhangolive':
+    Shudong = leancloud.Object.extend('shudong_test')
+else:
+    Shudong = leancloud.Object.extend('shudong')
+
+query = Shudong.query
+
+DOUBAN_TYPE = 'douban'
+
+
+def write_single_douban_shudong(text, published_at, photos=None):
+    shudong = Shudong()
+    shudong.set('content', text)
+    shudong.set('type', DOUBAN_TYPE)
+    shudong.set('published_at', published_at)
+    if photos:
+        shudong.set('photos', photos)
+    shudong.save()
+
 # 获取所有的电影信息
-for entry in douban_rss.entries:
-    print(entry)
-    title = entry.title
+def get_all_douban_status():
+    query.equal_to('type', DOUBAN_TYPE)
+    query.exists('published_at')
+    query.descending('published_at')
+    result = query.find()
+    for entry in douban_rss.entries:
+        title = entry.title
+        description = entry.description
+        published = entry.published
+        link = entry.link
+        soup = BeautifulSoup(description, 'html.parser')
+        text = title + soup.get_text()
+        published_at = datetime.datetime.strptime(published, "%a, %d %b %Y %H:%M:%S %Z").replace(tzinfo=datetime.timezone.utc)
+        if not result:
+            print('write', text)
+            write_single_douban_shudong(text, published_at)
+        else:
+            last_published_at = result[0].get('published_at')
+            if published_at > last_published_at:
+                print('write:', text)
+                write_single_douban_shudong(text, published_at)
 
-    description = entry.description
 
-    
-    published = entry.published
-
-    link = entry.link
-
-    print(title, description, link, published)
+get_all_douban_status()
 
 
-# If some 设置树洞类型，type=douban . 自定义pubdate ..比较pubdate，大于上一个pubdate的才存起来。
---
+++ shudong/lunar.py
@@ -0,0 +1,8 @@
+from lunar import Lunar  # 导入Lunar模块 
+# 创建Lunar对象，传入阳历年月日 
+# l = Lunar(1993, 1, 14)   # 2020年3月8日 
+# 获取农历信息 
+# lunarday = l.get_lunar()           # (2020, 2, 1)  2020年正月初一
+# print(lunaray) 
+# 获取两个生日相同的年份 
+# same_year = l.get_same_year()   # 2020 
