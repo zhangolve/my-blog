00b0f30c7c843e6003da392d4be594c449a23332
zhangolve <zhangolve@users.noreply.github.com>
2023-06-07 17:25:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,17 @@ directory="hexo-blog"
 
 rm -rf hexo-blog
 
-if [ ! -d "$directory" ]; then    
-    mkdir hexo-blog
-    cd hexo-blog
-    hexo init
-    npm install hexo-deployer-git --save
-    npm install hexo-generator-feed --save
-    npm install hexo-generator-sitemap --save
-    git clone https://github.com/theme-next/hexo-theme-next themes/next
-    rm -rf source/_posts/*
-    cd ../
-else
-    echo "目录已存在：$directory"
-fi
-
-cp ./hexo/* . -r
-cp ./2020/  source/_posts/ -r
-cp ./2021/  source/_posts/ -r
-cp ./2022/  source/_posts/ -r
-cp ./2023/  source/_posts/ -r
+mkdir hexo-blog
+cd hexo-blog
+hexo init
+npm install hexo-deployer-git --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-sitemap --save
+git clone https://github.com/theme-next/hexo-theme-next themes/next
+rm -rf source/_posts/*
+        
+cp ../hexo/* . -r
+cp ../2020/  source/_posts/ -r
+cp ../2021/  source/_posts/ -r
+cp ../2022/  source/_posts/ -r
+cp ../2023/  source/_posts/ -r
