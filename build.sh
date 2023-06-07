db44712ad6d863b6eb0893f7cc6ff42915ebd782
zhangolve <zhangolve@users.noreply.github.com>
2023-06-07 17:06:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 cd hexo-blog
 cp ../hexo/_next_theme_config.yml themes/next/_config.yml
+env_vars=$(printenv)
+
+# 逐行输出环境变量
+while IFS= read -r line; do
+  echo "$line"
+done <<< "$env_vars"
+
 env_variable="valine"
 valine="$($env_variable)"
 echo "$valine" >> themes/next/_config.yml
