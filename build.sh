3c1a3aeff7dcdde268f68ba6d6b4057bde4b1f9b
zhangolve <zhangolve@users.noreply.github.com>
2023-06-07 17:05:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 cd hexo-blog
+cp ../hexo/_next_theme_config.yml themes/next/_config.yml
+env_variable="valine"
+valine="$($env_variable)"
+echo "$valine" >> themes/next/_config.yml
 echo "hexo clean start"
 hexo clean
 echo "hexo clean end"
